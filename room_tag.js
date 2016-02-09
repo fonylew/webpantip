d45037cecd63fb00e3e41c3b1856f0f1d088cc0b
sirikorn <kornn.g@live.com>
2016-02-09 21:03:34 +0700

    room complete

--- room_tag.js
+++ room_tag.js
@@ -1,23 +1,36 @@
-$('#room').append("<button type='button' id='art' class='btn btn-primary btn-md'>หอศิลป์</button>\
-	<button type='button' id='bangrak' class='btn btn-primary btn-md'>บางรัก</button>\
-	<button type='button' id='beauty' class='btn btn-primary btn-md'>โต๊ะเครื่องแป้ง</button>\
-	<button type='button' id='blueplanet' class='btn btn-primary btn-md'>บลูแพลนเน็ต</button>\
-	<button type='button' id='camera' class='btn btn-primary btn-md'>กล้อง</button>\
-	<button type='button' id='cartoon' class='btn btn-primary btn-md'>การ์ตูน</button>\
-	<button type='button' id='chalermkrung' class='btn btn-primary btn-md'>เฉลิมกรุง</button>\
-	<button type='button' id='chalermthai' class='btn btn-primary btn-md'>เฉลิมไทย</button>\
-	<button type='button' id='family' class='btn btn-primary btn-md'>ชานเรือน</button>\
-	<button type='button' id='food' class='btn btn-primary btn-md'>ก้นครัว</button>\
-	<button type='button' id='gallery class='btn btn-primary btn-md'>แกลเลอรี่</button>\
-	<button type='button' id='greenzone' class='btn btn-primary btn-md'>กรีนโซน</button>\
-	<button type='button' id='home' class='btn btn-primary btn-md'>ชายคา</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
-	<button type='button' id='hideRoom' class='btn btn-primary btn-md'>ราชดำเนิน</button>\
+$('#room').append("<button type='button' id='art' class='btn btn-primary btn-sm'>หอศิลป์</button>\
+	<button type='button' id='bangrak' class='btn btn-primary btn-sm'>บางรัก</button>\
+	<button type='button' id='beauty' class='btn btn-primary btn-sm'>โต๊ะเครื่องแป้ง</button>\
+	<button type='button' id='blueplanet' class='btn btn-primary btn-sm'>บลูแพลนเน็ต</button>\
+	<button type='button' id='camera' class='btn btn-primary btn-sm'>กล้อง</button>\
+	<button type='button' id='cartoon' class='btn btn-primary btn-sm'>การ์ตูน</button>\
+	<button type='button' id='chalermkrung' class='btn btn-primary btn-sm'>เฉลิมกรุง</button>\
+	<button type='button' id='chalermthai' class='btn btn-primary btn-sm'>เฉลิมไทย</button>\
+	<button type='button' id='family' class='btn btn-primary btn-sm'>ชานเรือน</button>\
+	<button type='button' id='food' class='btn btn-primary btn-sm'>ก้นครัว</button>\
+	<button type='button' id='gallery' class='btn btn-primary btn-sm'>แกลเลอรี่</button>\
+	<button type='button' id='greenzone' class='btn btn-primary btn-sm'>กรีนโซน</button>\
+	<button type='button' id='home' class='btn btn-primary btn-sm'>ชายคา</button>\
+	<button type='button' id='horoscope' class='btn btn-primary btn-sm'>พรหมชาติ</button>\
+	<button type='button' id='jatujak' class='btn btn-primary btn-sm'>จตุจักร</button>\
+	<button type='button' id='klaibann' class='btn btn-primary btn-sm'>ไกลบ้าน</button>\
+	<button type='button' id='library' class='btn btn-primary btn-sm'>ห้องสมุด</button>\
+	<button type='button' id='lumpini' class='btn btn-primary btn-sm'>สวนลุมพินี</button>\
+	<button type='button' id='mbk' class='btn btn-primary btn-sm'>มาบุญครอง</button>\
+	<button type='button' id='pantip' class='btn btn-primary btn-sm'>พันทิป</button>\
+	<button type='button' id='hideRoom' class='btn btn-primary btn-sm'>ราชดำเนิน</button>\
+	<button type='button' id='ratchada' class='btn btn-primary btn-sm'>รัชดา</button>\
+	<button type='button' id='region' class='btn btn-primary btn-sm'>ภูมิภาค</button>\
+	<button type='button' id='religious' class='btn btn-primary btn-sm'>ศาสนา</button>\
+	<button type='button' id='siam' class='btn btn-primary btn-sm'>สยาม</button>\
+	<button type='button' id='siliconvalley' class='btn btn-primary btn-sm'>ซิลิคอนวัลเลย์</button>\
+	<button type='button' id='silom' class='btn btn-primary btn-sm'>สีลม</button>\
+	<button type='button' id='sinthorn' class='btn btn-primary btn-sm'>สินธร</button>\
+	<button type='button' id='social' class='btn btn-primary btn-sm'>ศาลาประชาคม</button>\
+	<button type='button' id='supachalasai' class='btn btn-primary btn-sm'>ศุภาชลาศัย</button>\
+	<button type='button' id='tvshow' class='btn btn-primary btn-sm'>บางขุนพรหม</button>\
+	<button type='button' id='wahkor' class='btn btn-primary btn-sm'>หว้ากอ</button>\
+	<button type='button' id='writer' class='btn btn-primary btn-sm'>ถนนนักเขียน</button><br>\
+	<div class='col-md-offset-10'><button type='button' id='hideroom' class='btn btn-success btn-sm'>hide</button></div>\
 	");
 
