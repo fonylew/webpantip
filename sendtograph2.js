550f25cdfe36a7529b78a3e3b71000fb88ce2ee2
sirikorn <kornn.g@live.com>
2016-02-24 22:48:07 +0700

    bug sendtograph2 dont show graph

--- sendtograph2.js
+++ sendtograph2.js
@@ -1,5 +1,5 @@
-    function sendToGraph(room,type){
-    alert(room+" "+type);
+function sendToGraph2(user){
+    alert(user);
          var g = {
             nodes: [],
             edges: []
@@ -15,7 +15,7 @@
     var s = new sigma({
       graph: g,
       renderer: {
-        container: document.getElementById('graph-container'),
+        container: document.getElementById('graph-container2'),
         type: 'canvas'
       },
         settings: {
@@ -25,11 +25,11 @@
 
     sigma.neo4j.cypher(
             { url: 'http://localhost:7474', user: 'neo4j', password: 'root' },
-            'match (u:User)-[:POSTED]->(p:Topic)<-[r:REPLIED]-(),(p:Topic)-[c:CLASSED]->(g:Room{name: "'+room+'"}) return u,p,count(r) as DegreeScore order by DegreeScore desc limit 10;',
+            'match (u:User{id:993778})-[v:POSTED]->(p:Topic)<-[r:REPLIED]-(o:User) return u,p,o,v,r;',
            s,//match (u:User)-[:POSTED]->(p:Topic)<-[r:REPLIED]-(),(p:Topic)-[c:CLASSED]->(g:Room{name: "'+room+'"}) return u,p,count(r) as DegreeScore order by DegreeScore desc limit 10;
             function() {
 
-                console.log('Number of nodes :'+ s.graph.nodes().length);
+                console.log('Number of nodes2222 :'+ s.graph.nodes().length);
                 var i,
                 nodes = s.graph.nodes(),
                 lenN = nodes.length;
@@ -42,13 +42,13 @@
                       nodes[i].color = colors[0];
                       nodes[i].label = nodes[i].neo4j_data['id'];
                       data[i] = nodes[i].label;
-                      console.log(nodes[i].label +" User");
+                      console.log(nodes[i].label +" User222222");
                     } 
               
                     if(nodes[i].neo4j_labels == "Topic"){
                      nodes[i].color = colors[1];
                      nodes[i].label = nodes[i].neo4j_data['id'];
-                     console.log(nodes[i].label +" Topic");
+                     console.log(nodes[i].label +" Topic22222222");
                       //nodes[i].label = nodes[i].neo4j_data['id'];
                    }
                     if(nodes[i].neo4j_labels == "Room") nodes[i].color = colors[2];
@@ -56,7 +56,7 @@
                     //nodes[i].color = colors[Math.floor(Math.random() * colors.length)];
 
                 }
-                console.log('Number of edges :'+ s.graph.edges().length);
+                console.log('Number of edges222 :'+ s.graph.edges().length);
                 var j,
                 edges = s.graph.edges(),
                 lenE = edges.length;
@@ -91,37 +91,9 @@
               /*  setTimeout(function () {
                 s.killForceAtlas2();
                       }, 1800);*/
-              sentDataToArray(data);
             }
     );
 
 
-    // Calling neo4j to get all its relationship type
-    sigma.neo4j.getTypes(
-            { url: 'http://localhost:7474', user:'neo4j', password:'root' },
-            function(types) {
-                console.log("Relationship types " + types);
-            }
-    );
 
-    // Calling neo4j to get all its node label
-    sigma.neo4j.getLabels(
-            { url: 'http://localhost:7474', user:'neo4j', password:'root' },
-            function(labels) {
-                console.log("Node labels " + labels);
-            }
-    );
-
-    /*
-    for(var i = 0 ; i < 10 ;i++) {
-    $('#user_list'+i+'').append(data[i]);
-    }*/
-    $('#room_type').append(room+" "+type);
 }
-function sentDataToArray(data){
-     for(var i = 0 ; i < 10 ;i++) {
-    $('#user_list'+i+'').append('<a href="http://pantip.com/profile/'+data[i]+'">'+data[i]+'</a> <button  onclick=\"sendUser('+i+')\"> view graph </button>\
-    <input id="huser_list'+i+'" type="hidden" value ="'+data[i]+'"></input>');
-  }
-}
-
